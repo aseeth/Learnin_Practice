3837696b9ff3310765528b5c67496944bec3f1d0
aseeth <aseeth.b@gmail.com>
2020-12-01 11:18:33 +0530

    connect MySql

---
+++ Python/Recall_All.py
@@ -0,0 +1,23 @@
+###Recall the all Basics###
+
+
+#in python every thing is an object
+#s = "hello"
+#print(s.capitalize())
+#print("hello".capitalize())
+
+#Boolean
+#print('a'>'Z')
+print('ab' > 'b')
+print(ord('a'),ord('Z')) #ord() method for find AASCI value
+print(chr(97))           # chr() method for find charecter for that value
+
+
+'''
+list = [] #empty list
+list1 = [30,40,60]
+list = [1,2,3]
+list.insert(0,10) #insert element by index
+list.append(20)   #insert element at end of the list
+list2=list+list1  #adding two lists
+print(list2)'''       
